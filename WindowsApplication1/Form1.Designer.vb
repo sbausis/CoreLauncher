2ab8850b304dd8a0e1a2988be9cc40ed075c91d7
Simon Pascal Baur <sbausis@gmx.net>
2016-04-24 00:46:58 +0200

    initial commit

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButtonSettings = New System.Windows.Forms.Button()
+        Me.ButtonToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ButtonFirefox = New System.Windows.Forms.Button()
+        Me.ButtonCMD = New System.Windows.Forms.Button()
+        Me.ButtonPowershell = New System.Windows.Forms.Button()
+        Me.ButtonFileExplorer = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonSettings
+        '
+        Me.ButtonSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonSettings.Image = CType(resources.GetObject("ButtonSettings.Image"), System.Drawing.Image)
+        Me.ButtonSettings.Location = New System.Drawing.Point(0, 0)
+        Me.ButtonSettings.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonSettings.Name = "ButtonSettings"
+        Me.ButtonSettings.Size = New System.Drawing.Size(36, 36)
+        Me.ButtonSettings.TabIndex = 0
+        Me.ButtonSettings.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.ButtonSettings, "Settings")
+        Me.ButtonSettings.UseVisualStyleBackColor = False
+        '
+        'ButtonToolTip
+        '
+        Me.ButtonToolTip.AutomaticDelay = 10
+        Me.ButtonToolTip.AutoPopDelay = 0
+        Me.ButtonToolTip.BackColor = System.Drawing.SystemColors.Desktop
+        Me.ButtonToolTip.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.ButtonToolTip.InitialDelay = 10
+        Me.ButtonToolTip.ReshowDelay = 0
+        Me.ButtonToolTip.ShowAlways = True
+        Me.ButtonToolTip.UseAnimation = False
+        Me.ButtonToolTip.UseFading = False
+        '
+        'ButtonFirefox
+        '
+        Me.ButtonFirefox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonFirefox.Image = CType(resources.GetObject("ButtonFirefox.Image"), System.Drawing.Image)
+        Me.ButtonFirefox.Location = New System.Drawing.Point(0, 36)
+        Me.ButtonFirefox.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonFirefox.Name = "ButtonFirefox"
+        Me.ButtonFirefox.Size = New System.Drawing.Size(36, 36)
+        Me.ButtonFirefox.TabIndex = 1
+        Me.ButtonFirefox.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.ButtonFirefox, "Firefox")
+        Me.ButtonFirefox.UseVisualStyleBackColor = True
+        '
+        'ButtonCMD
+        '
+        Me.ButtonCMD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonCMD.Image = CType(resources.GetObject("ButtonCMD.Image"), System.Drawing.Image)
+        Me.ButtonCMD.Location = New System.Drawing.Point(0, 72)
+        Me.ButtonCMD.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonCMD.Name = "ButtonCMD"
+        Me.ButtonCMD.Size = New System.Drawing.Size(36, 36)
+        Me.ButtonCMD.TabIndex = 2
+        Me.ButtonCMD.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.ButtonCMD, "Command Line")
+        Me.ButtonCMD.UseVisualStyleBackColor = False
+        '
+        'ButtonPowershell
+        '
+        Me.ButtonPowershell.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonPowershell.Image = CType(resources.GetObject("ButtonPowershell.Image"), System.Drawing.Image)
+        Me.ButtonPowershell.Location = New System.Drawing.Point(0, 108)
+        Me.ButtonPowershell.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonPowershell.Name = "ButtonPowershell"
+        Me.ButtonPowershell.Size = New System.Drawing.Size(36, 36)
+        Me.ButtonPowershell.TabIndex = 3
+        Me.ButtonPowershell.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.ButtonPowershell, "Powershell")
+        Me.ButtonPowershell.UseVisualStyleBackColor = False
+        '
+        'ButtonFileExplorer
+        '
+        Me.ButtonFileExplorer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonFileExplorer.Image = CType(resources.GetObject("ButtonFileExplorer.Image"), System.Drawing.Image)
+        Me.ButtonFileExplorer.Location = New System.Drawing.Point(0, 144)
+        Me.ButtonFileExplorer.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonFileExplorer.Name = "ButtonFileExplorer"
+        Me.ButtonFileExplorer.Size = New System.Drawing.Size(36, 36)
+        Me.ButtonFileExplorer.TabIndex = 4
+        Me.ButtonFileExplorer.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.ButtonFileExplorer, "A43 File Explorer")
+        Me.ButtonFileExplorer.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(0, 180)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(36, 36)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.Button1, "Settings")
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(0, 216)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(36, 36)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Tag = ""
+        Me.ButtonToolTip.SetToolTip(Me.Button2, "Settings")
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(36, 252)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ButtonFileExplorer)
+        Me.Controls.Add(Me.ButtonPowershell)
+        Me.Controls.Add(Me.ButtonCMD)
+        Me.Controls.Add(Me.ButtonFirefox)
+        Me.Controls.Add(Me.ButtonSettings)
+        Me.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(36, 256)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(36, 252)
+        Me.Name = "Form1"
+        Me.Opacity = 0.6R
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.TransparencyKey = System.Drawing.SystemColors.Control
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ButtonSettings As Button
+    Friend WithEvents ButtonToolTip As ToolTip
+    Friend WithEvents ButtonFirefox As Button
+    Friend WithEvents ButtonCMD As Button
+    Friend WithEvents ButtonPowershell As Button
+    Friend WithEvents ButtonFileExplorer As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub ButtonFirefox_Click(sender As Object, e As EventArgs) Handles ButtonFirefox.Click
+        Dim startInfo As New ProcessStartInfo
+        startInfo.FileName = "C:\Program Files\FirefoxPortable\FirefoxPortable.exe"
+        startInfo.Arguments = "localhost"
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = "C:\"
+        Process.Start(startInfo)
+    End Sub
+    Private Sub ButtonCMD_Click(sender As Object, e As EventArgs) Handles ButtonCMD.Click
+        Dim startInfo As New ProcessStartInfo
+        startInfo.FileName = "C:\Windows\System32\cmd.exe"
+        startInfo.Arguments = ""
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = "C:\"
+        Process.Start(startInfo)
+    End Sub
+    Private Sub ButtonPowershell_Click(sender As Object, e As EventArgs) Handles ButtonPowershell.Click
+        Dim startInfo As New ProcessStartInfo
+        startInfo.FileName = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+        startInfo.Arguments = ""
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = "C:\"
+        Process.Start(startInfo)
+    End Sub
+    Private Sub ButtonFileExplorer_Click(sender As Object, e As EventArgs) Handles ButtonFileExplorer.Click
+        Dim startInfo As New ProcessStartInfo
+        startInfo.FileName = "C:\Program Files (x86)\A43\A43.exe"
+        startInfo.Arguments = ""
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = "C:\"
+        Process.Start(startInfo)
+    End Sub
+End Class
